2ca8d9bcd68db2d91a37eec1e4a35bb5e53b9415
HelaniUdara <seekkub-se19022@stu.kln.ac.lk>
2023-03-23 19:29:29 +0530

    Created header

---
+++ front-end/assignment/src/common/header/Head.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Head = ()=> {
+    return (
+        <>
+         <section className="head">
+         <div className="container d_flex">
+                    <div className="left row">
+                        <i className="fa fa-phone"></i>
+                        <lable>+94 153 234 679</lable>
+                        <i className="fa fa-envelope"></i>
+                        <lable>thunderBolt@gmail.com</lable>
+                    </div>
+                    <div className="right row RText">
+                        <lable>Help</lable>
+                        <lable>FAQ's</lable>
+                    </div>
+                </div>
+         </section>
+        </>
+    )
+}
+
+export default Head
---
+++ front-end/assignment/src/common/header/Navbar.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = ()=> {
+    const [MobileMenu, setMobileMenu] = useState(false)
+    return (
+        <>
+         <header className="header">
+                <div className="container">
+                    <div className="categories d_flex">
+                        <div className="categories d_flex">
+                            <span className="fa fa-plus-square-o"></span>
+                            <h4>
+                                Categories <i className="fa fa-chevron-down"></i>
+                            </h4>
+                        </div>
+                        <div className="navlink">
+                            <ul className={MobileMenu? "nav-links-MobileMenu" : "link f_flex capitalize"} onClick={() => setMobileMenu(false)}>
+                                <li>
+                                    <Link to="/">Home</Link>
+                                </li>
+                                <li>
+                                    <Link to="/user">User Account</Link>
+                                </li>
+                                <li>
+                                    <Link to="/track">Track My Order</Link>
+                                </li>
+                                <li>
+                                    <Link to="/contact">Contact</Link>
+                                </li>
+                                    
+                                    
+                                                                                        
+                           </ul>
+                            <button className="toggle" onClick={() => setMobileMenu(!MobileMenu)}>
+                                {
+                                    MobileMenu? <i className="fa fa-times close home-btn"></i> :
+                                    <i className="fa fa-bars open"></i>
+                                }
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Navbar
